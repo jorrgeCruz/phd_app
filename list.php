d480401912efb5c74b4c1f6c6764c07e6ecb9da5
Jorge Cruz <jorrge.cruz@gmail.com>
2022-12-07 12:10:37 -0600

    REgistro de avance de la primera semana

--- list.php
+++ list.php
@@ -15,145 +15,30 @@ $query = mysqli_query($conn, $sql_fetch_todos);
 <html lang="en">
 
 <head>
-    <title>Lista de Productos</title>
+    <title>Lista de Lenguajes</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="icon" href="faviconconfiguroweb.png">
     <link href="https://fonts.googleapis.com/css2?family=Mitr&display=swap" rel="stylesheet">
-    <style>
-        body {
-            font-family: Arial, Helvetica, sans-serif;
-            background-color: #fd7e1b;
-        }
-        .header {
-            position: fixed;
-            top: 0px;
-            left: 0px;
-            right: 0px;
-            height: 50px;
-            padding: 5px 13px 11px 0px;
-            width: 100%;
-            color: white;
-            font-family: Arial, Helvetica, sans-serif;
-            margin-top: 0px;
-            bottom: 0;
-            background-color: #298dba;
-        }
-        .header p {
-            margin-left: 20px;
-            text-align: left;
-        }
-        .button-logout {
-            position: relative;
-            margin-top: -50px;
-            margin-right: 20px;
-            float: right;
-            text-decoration: none;
-            border: transparent;
-            border-radius: 15px;
-            background-color: #e60000;
-            padding: 10px 10px 10px 10px;
-            color: white;
-            transition: 0.5s;
-        }
-        .button-logout:hover {
-            background-color: #D9ddd4;
-            color: red;
-        }
-        .container {
-            margin: 90px auto;
-            margin-bottom: 50px;
-            border-radius: 30px;
-            text-align: center;
-            background-color: white;
-            width: 40%;
-            padding-bottom: 10px;
-        }
-        table th,
-        tr,
-        td {
-            border: 1px solid black;
-            border-collapse: collapse;
-            padding: 10px 0px 10px 0px;
-        }
-        table {
-            width: 100%;
-        }
-        th {
-            color: white;
-            background-color: #298dba;
-        }
-        tr {
-            background-color: white;
-        }
-        tr:nth-child(even) {
-            background-color: #f2f2f2;
-        }
-        .timeregis {
-            text-align: center;
-        }
-        .modify {
-            text-align: center;
-        }
-        .delete {
-            text-align: center;
-        }
-        .modify .bfix {
-            border-radius: 15px;
-            background-color: #ffcc33;
-            color: black;
-            text-decoration: none;
-            padding: 4px 20px 4px 20px;
-            transition: 0.5s;
-        }
-        .modify .bfix:hover {
-            background-color: #fdb515;
-            color: white;
-        }
-        .delete .bdelete {
-            border-radius: 15px;
-            background-color: #e60000;
-            text-decoration: none;
-            color: white;
-            padding: 4px 20px 4px 20px;
-            transition: 0.5s;
-        }
-        .delete .bdelete:hover {
-            background-color: #D9ddd4;
-            color: red;
-        }
-        .Addlist {
-            margin-right: 100px;
-            padding: 5px 30px 5px 30px;
-            border-radius: 15px;
-            text-decoration: none;
-            color: white;
-            background-color: #00A600;
-            transition: 0.5s;
-        }
-        .Addlist:hover {
-            color: black;
-            background-color: #BBFFBB;
-        }
-    </style>
+    <link rel="stylesheet" href="./style/list.css">
 </head>
 <body>
     <div class="header">
-        <h3>ConfiguroWeb</h3>
+        <h3>Unisur - Doctorado en Sistemas Computacionales</h3>
         <a name="" id="" class="button-logout" href="logout.php" role="button">Cerrar Sesión</a>
     </div>
     <div class="container">
-        <h1>Lista de Productos</h1>
+        <h1>Lista de Lenguajes de Programacion</h1>
         <h2>Has accedido como <?php echo $str = strtoupper($username) ?></h2>
     </div>
     <div class="table-product">
         <table>
             <tr>
                 <th scope="col">Orden</th>
-                <th scope="col">ID:Producto</th>
-                <th scope="col">Nombre:Producto</th>
-                <th scope="col">Cantidades</th>
+                <th scope="col">ID:Lenguaje</th>
+                <th scope="col">Nombre:Lenguaje</th>
+                <th scope="col">No Veces <br> registrado </th>
                 <th scope="col">Fecha:Registro</th>
                 <th scope="col">Editar</th>
                 <th scope="col">Eliminar</th>
@@ -181,7 +66,9 @@ $query = mysqli_query($conn, $sql_fetch_todos);
             </tbody>
         </table>
         <br>
-        <a name="" id="" class="Addlist" style="float:right" href="addlist.php" role="button">Agregar Producto</a>
+        <div class="bottom-button">
+            <a name="" id="" class="Addlist"  href="addlist.php" role="button">Agregar Producto</a>
+        </div>
     </div>
     <?php
     mysqli_close($conn);
