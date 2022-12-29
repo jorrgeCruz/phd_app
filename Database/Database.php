10543f510420bfed23dfe917cc430d0249b0728b
Jorge Cruz <jorrge.cruz@gmail.com>
2022-12-28 21:18:59 -0600

    Avance semana 3, capa de datos con bd externa

--- Database/Database.php
+++ Database/Database.php
@@ -6,6 +6,6 @@
     $conn = new mysqli($host , $user, $pass, $dbname);
     mysqli_query($conn , "SET character_set_result=utf8");
     if($conn->connect_error){
-        die("Database Error : " . $conn->connect_error);
+        die("Database Error: " . $conn->connect_error);
     }
 ?>
